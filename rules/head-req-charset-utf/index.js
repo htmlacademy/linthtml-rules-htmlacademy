89a0ce49d0450e7c9c0ff05569f0461dfbb3a9b8
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-06-28 10:58:20 +0300

    Add new rule `head-req-charset-utf`

---
+++ rules/head-req-charset-utf/index.js
@@ -0,0 +1,18 @@
+const dom_utils = require("@linthtml/dom-utils");
+
+module.exports = {
+  name: "htmlacademy/head-req-charset-utf",
+  lint(node, rule_config, { report }) {
+    if (dom_utils.is_tag_node(node) && node.name === "meta") {
+      const hasUtf = node.attributes.some(attribute => attribute.value.chars.toLowerCase() === 'utf-8');
+      const hasCharset = node.attributes.some(attribute => attribute.name.chars === "charset");
+
+      if (hasCharset === true && hasUtf === false) {
+        report({
+          position: node.loc,
+          message: `"The attribute charset="" should have a value of 'utf-8'."`,
+        });
+      }
+    }
+  }
+}
