b5fee844aa1a642e92d6f68d31a7b01050695d4a
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-06-28 09:57:11 +0300

    Loads components and dynamically creates rules

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-const dom_utils = require("@linthtml/dom-utils");
-const check_format = (value) => /^([a-z\d]+(-[a-z\d]+)*)(\s[a-z\d]+(-[a-z\d]+)*)*$/.test(value);
+const fs = require('fs');
+const path = require('path');
+
+const rulesDir = path.join(__dirname, './rules');
+const ruleFiles = fs.readdirSync(rulesDir);
+const rules = ruleFiles.map((file) => require(path.join(rulesDir, file)));
+
 module.exports = {
-  rules: [
-    {
-      name: "htmlacademy/class-first",
-      lint(node, rule_config, { report }) {
-        if (dom_utils.is_tag_node(node) === false) {
-          return;
-        }
-        const attribute = dom_utils.get_attribute(node, 'class');
-        if (attribute && attribute !== node.attributes[0]) {
-          report({
-            position: attribute.loc,
-            message: `The class attribute should be the first.`,
-          });
-        }
-      }
-    },
-    {
-      name: "htmlacademy/head-req-meta",
-      lint(node, rule_config, { report }) {
-        if (dom_utils.is_tag_node(node) && node.name === "head") {
-          const meta = node.children.filter((child) => child.name === "meta");
-          const hasCharset = meta.some((meta) => {
-            return meta.attributes.some((attribute) => {
-              return attribute.name.chars === "charset";
-            });
-          });
-          if (hasCharset === false) {
-            report({
-              position: node.loc,
-              message: `The <meta> tag should have a charset attribute.`,
-            });
-          }
-        }
-      }
-    }
-  ]
+  rules
 };
