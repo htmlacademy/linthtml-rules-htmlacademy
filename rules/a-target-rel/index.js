180a63243f2b9d96809195ef971923ac0a711b1d
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-06-29 22:43:37 +0300

    Resurrect code

--- rules/a-target-rel/index.js
+++ rules/a-target-rel/index.js
@@ -1,5 +1,4 @@
 const { is_tag_node, attribute_has_value } = require("@linthtml/dom-utils");
-
 const requiredAttributes = {
   // todo вынести в конфиг правила
   rel: ["nofollow", "noopener"]
@@ -11,7 +10,7 @@ const isEveryValuePresent = (node, attr, values) => {
 
 const isExternalLink = (node) => {
   return is_tag_node(node) && node.name === "a" &&
-      attribute_has_value(node, 'target', '_blank')
+    attribute_has_value(node, 'target', '_blank')
 }
 
 module.exports = {
@@ -26,55 +25,6 @@ module.exports = {
           });
         }
       }
-    }
-  },
-};
-
-const requiredAttributes = [
-  { name: 'rel', value: 'nofollow' },
-  { name: 'rel', value: 'noopener' },
-];
-
-function isMissingAttribute(node, attribute) {
-  return !node.attributes.some((attr) => (
-    attr.name.chars === attribute.name && attr.value.chars.includes(attribute.value)
-  ));
-}
-module.exports = {
-  name: "htmlacademy/a-target-rel",
-  lint(node, rule_config, { report }) {
-    if (
-      dom_utils.is_tag_node(node) &&
-      node.name === "a" &&
-      node.attributes.some(
-        (attr) => attr.name.chars === "target" && attr.value.chars === "_blank"
-      )
-    ) {
-      const requiredAttributes = [
-        { name: "rel", value: "nofollow" },
-        { name: "rel", value: "noopener" },
-      ];
-      const missingAttributes = requiredAttributes.filter((attribute) =>
-        !node.attributes.some(
-          (attr) =>
-            attr.name.chars === attribute.name &&
-            attr.value.chars.includes(attribute.value)
-        )
-      );
-requiredAttributes
-        .filter((attribute) => isMissingAttribute(node, attribute))
-        .forEach((attribute) => {
-          report({
-            position: node.loc,
-            message: `The <a> element with target="_blank" is missing the "${attribute.name}=${attribute.value}" attribute.`,
-          });
-        });
-      missingAttributes.forEach((attribute) => {
-        report({
-          position: node.loc,
-          message: `The <a> element with target="_blank" is missing the "${attribute.name}=${attribute.value}" attribute.`,
-        });
-      });
     }
   },
 };
