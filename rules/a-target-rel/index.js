8842572bf606529ce83ab2ef0a40c15122d43846
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-06-29 22:21:36 +0300

    Update rules/a-target-rel/index.js
    Co-authored-by: balesniy <balesniy@yandex.ru>

--- rules/a-target-rel/index.js
+++ rules/a-target-rel/index.js
@@ -1,4 +1,35 @@
-const dom_utils = require("@linthtml/dom-utils");
+const { is_tag_node, attribute_has_value } = require("@linthtml/dom-utils");
+
+const requiredAttributes = {
+  // todo вынести в конфиг правила
+  rel: ["nofollow", "noopener"]
+};
+
+const isEveryValuePresent = (node, attr, values) => {
+  return values.every(val => attribute_has_value(node, attr, val))
+}
+
+const isExternalLink = (node) => {
+  return is_tag_node(node) && node.name === "a" &&
+      attribute_has_value(node, 'target', '_blank')
+}
+
+module.exports = {
+  name: "htmlacademy/a-target-rel",
+  lint(node, rule_config, { report }) {
+    if (isExternalLink(node)) {
+      for (const [attr, values] of Object.entries(requiredAttributes)) {
+        if (!isEveryValuePresent(node, attr, values)) {
+          report({
+            position: node.loc,
+            message: `The <a> element with target="_blank" is missing the "${attr}=${values.join(' ')}" attribute.`
+          });
+        }
+      }
+    }
+  },
+};
+
 const requiredAttributes = [
   { name: 'rel', value: 'nofollow' },
   { name: 'rel', value: 'noopener' },
