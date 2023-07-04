463dc9f3138c70382e527f6059bf318df7f94e7a
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-07-04 22:15:08 +0300

    Checks attribute values for emptiness

--- rules/img-svg-req-dimensions/index.js
+++ rules/img-svg-req-dimensions/index.js
@@ -1,14 +1,12 @@
-const dom_utils = require("@linthtml/dom-utils");
+const {is_tag_node, has_non_empty_attribute} = require("@linthtml/dom-utils");
 
 module.exports = {
   name: "htmlacademy/img-svg-req-dimensions",
   lint(node, rule_config, { report }) {
-    if (dom_utils.is_tag_node(node) && (node.name === "img" || node.name === "svg")) {
+    if (is_tag_node(node) && (node.name === "img" || node.name === "svg")) {
       const requiredAttributes = ["width", "height"];
       const missingAttributes = requiredAttributes.filter((attribute) =>
-          !node.attributes.some(
-            (attr) => attr.name.chars === attribute
-          )
+        !has_non_empty_attribute(node, attribute)
       );
 
       if (missingAttributes.length > 0) {
