436cdb4c88019e2a1d43cc9ab097b9a70651a89e
balesniy <balesniy@yandex.ru>
2023-07-04 22:41:12 +0200

    no-double-br

---
+++ rules/no-double-br/index.js
@@ -0,0 +1,29 @@
+const { is_tag_node } = require("@linthtml/dom-utils");
+
+function checkPrevNode(node) {
+  if (!node.prev) {
+    return false
+  }
+  if (is_tag_node(node.prev)) {
+    return node.prev.name === 'br'
+  }
+  if (node.prev.data.trim()) {
+    return false
+  }
+  return checkPrevNode(node.prev)
+}
+
+module.exports = {
+  name: "htmlacademy/no-double-br",
+  lint(node, rule_config, { report }) {
+    if (is_tag_node(node) === false || node.name !== 'br') {
+      return;
+    }
+    if (checkPrevNode(node)) {
+      report({
+        position: node.loc,
+        message: `do not use combined <br> tags`,
+      });
+    }
+  }
+};
