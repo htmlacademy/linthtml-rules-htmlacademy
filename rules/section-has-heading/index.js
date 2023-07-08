0b501d1f0bacd4f129cab36e179bca16836f1d33
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-07-08 12:11:10 +0300

    Adds htmlacademy/section-has-heading

---
+++ rules/section-has-heading/index.js
@@ -0,0 +1,31 @@
+const { is_tag_node } = require("@linthtml/dom-utils");
+
+const isSectionElement = (node) => is_tag_node(node) && node.name === "section";
+const isHeadingElement = (node) => is_tag_node(node) && /^h[1-6]$/.test(node.name);
+const checkChildNode = (node) => {
+  if (isHeadingElement(node)) {
+    return true;
+  }
+
+  if (node.children) {
+    for (const child of node.children) {
+      if (checkChildNode(child)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+module.exports = {
+  name: "htmlacademy/section-has-heading",
+  lint(node, rule_config, { report }) {
+    if (isSectionElement(node) && !checkChildNode(node)) {
+      report({
+        position: node.loc,
+        message: "The <section> element must contain a heading of any level.",
+      });
+    }
+  }
+};
