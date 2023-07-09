9102d5a4592283a0425789d0ec368fb8e13092eb
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2023-07-09 12:18:27 +0300

    Filters svg elements

--- rules/section-has-heading/index.js
+++ rules/section-has-heading/index.js
@@ -2,12 +2,13 @@ const { is_tag_node } = require("@linthtml/dom-utils");
 
 const isSectionElement = (node) => is_tag_node(node) && node.name === "section";
 const isHeadingElement = (node) => is_tag_node(node) && /^h[1-6]$/.test(node.name);
+const isNotSvg = (node) => node.name !== 'svg';
 const checkChildNode = (node) => {
   if (isHeadingElement(node)) {
     return true;
   }
 
-  if (node.children) {
+  if (node.children && isNotSvg(node)) {
     for (const child of node.children) {
       if (checkChildNode(child)) {
         return true;
