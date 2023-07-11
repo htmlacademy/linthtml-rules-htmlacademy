148ce3b02dc99231c3e3c4991899fd7a4c390362
balesniy <balesniy@yandex.ru>
2023-07-12 00:16:53 +0200

    attr-delimiter

---
+++ rules/attr-delimiter/index.js
@@ -0,0 +1,18 @@
+const { is_tag_node } = require("@linthtml/dom-utils");
+
+// node.attributes.map(a => a.equal.chars)
+module.exports = {
+  name: "htmlacademy/attr-delimiter",
+  lint(node, rule_config, { report }) {
+    if (is_tag_node(node)) {
+      node.attributes.forEach((attribute) => {
+        if (attribute.value?.chars.length > 0 && /\s/.test(attribute.equal.chars)) {
+          report({
+            position: attribute.loc,
+            message: "Attribute value must not be delimited by whitespace",
+          });
+        }
+      })
+    }
+  }
+};
