12f400059cc6eb0734fe6aeaf9978108ba9c52ce
balesniy <balesniy@yandex.ru>
2023-07-12 18:38:13 +0200

    fix id-no-dup

---
+++ rules/id-no-dup/README.md
@@ -0,0 +1,5 @@
+# htmlacademy/id-no-dup
+
+Форк: https://linthtml.vercel.app/user-guide/rules/list/id-no-dup
+
+Исправлена ошибка при сравнении `id` в нескольких файлах
---
+++ rules/id-no-dup/index.js
@@ -0,0 +1,36 @@
+const { is_tag_node, attribute_value, has_non_empty_attribute } = require("@linthtml/dom-utils");
+
+module.exports = {
+  name: "htmlacademy/id-no-dup",
+  idMap: new Map(),
+  lint(node, rule_config, { report }) {
+    if (is_tag_node(node) && has_non_empty_attribute(node, "id")) {
+      const id = attribute_value(node, "id");
+      if (/^¤+$/.test(id.chars)) {
+        return;
+      }
+      // node has a duplicate id
+      const saved_id = this.idMap.get(id.chars);
+      if (saved_id) {
+        report({
+          code: "E012",
+          position: id.loc,
+          meta: {
+            data: {
+              id: id.chars,
+              line: saved_id.loc.start.line,
+              column: saved_id.loc.start.column
+            }
+          }
+        });
+      }
+      // if we haven't seen the id before, remember it
+      this.idMap.set(id.chars, id);
+    }
+  },
+  end() {
+    // wipe previous table
+    this.idMap.clear();
+    return [];
+  }
+}
