62c291a3a650c5cf78c1d1e39368a3a5a8d1628b
balesniy <balesniy@yandex.ru>
2023-07-12 18:38:31 +0200

    fix input-req-label

---
+++ rules/input-req-label/README.md
@@ -0,0 +1,42 @@
+# htmlacademy/input-req-label
+
+Форк: https://linthtml.vercel.app/user-guide/rules/list/input-req-label
+
+Правило требует наличие метки для поля ввода, и позволяет указать метку в `aria-label`
+
+## true
+Если включён, то метки не могут быть пустыми.
+
+
+Проблемными считаются следующие шаблоны:
+```html
+<input type="text" value="great" >
+
+<div>
+  <label for="dinosaur">Label!</label>
+</div>
+<section>
+  <input type="text" id="romeo">
+</section>
+```
+
+Следующие шаблоны **не** считаются проблемами:
+
+```html
+<div>
+  <label for="dinosaur">Label!</label>
+</div>
+<section>
+  <input type="radio" id="dinosaur">
+</section>
+
+<label>
+  Label!
+  <input type="text">
+</label>
+
+<input type="text" aria-label="dinosaur">
+
+<input type="hidden" value="dinosaur">
+
+```
---
+++ rules/input-req-label/index.js
@@ -0,0 +1,98 @@
+const {
+  is_tag_node,
+  attribute_value,
+  attribute_has_value,
+  has_non_empty_attribute,
+} = require("@linthtml/dom-utils");
+
+class Issue {
+  code;
+  position;
+  rule;
+  message;
+  data = {};
+  severity = "error";
+
+  constructor(rule_name, position, options) {
+    this.position = position;
+    this.code = options.code;
+    this.rule = rule_name;
+    this.message = options.message ?? "";
+    this.data = options.data || {};
+    this.severity = options.severity || "error";
+  }
+}
+
+module.exports = {
+  name: "htmlacademy/input-req-label",
+  labels: {},
+  inputsInfo: [],
+  lint(node, rule_config, { report }) {
+    if (!is_tag_node(node) || !["input", "label"].includes(node.name)) {
+      return;
+    }
+    // if it's a label with a 'for', store that value
+    if (node.name === "label") {
+      const for_attribute = attribute_value(node, "for");
+      if (for_attribute) {
+        this.labels[for_attribute.chars] = node;
+      }
+      return;
+    }
+
+    if (attribute_has_value(node, "type", "hidden")) {
+      return;
+    }
+
+    if (has_non_empty_attribute(node, "aria-label")) {
+      return;
+    }
+
+    // check if the input has a label as a parent.
+    for (let e = node; (e = e.parent);) {
+      if (e.name === "label") {
+        return;
+      }
+    }
+
+    // check if the input has a named label, by storing the values to
+    // check at the end.
+    const id = attribute_value(node, "id");
+    if (id) {
+      this.inputsInfo.push({
+        id: id.chars, loc: node.open.loc,
+      });
+    } else {
+      report({
+        code: "E033", position: node.open.loc, meta: {
+          data: {
+            idValue: "null",
+          },
+        },
+      });
+    }
+  },
+  end() {
+    const issues = [];
+    const {
+      inputsInfo, labels,
+    } = this;
+    inputsInfo.forEach(({ id, loc }) => {
+      if (!labels[id]) {
+        issues.push(new Issue('input-req-label', loc, {
+          code: "E033", rule: 'input-req-label', data: {
+            idValue: id,
+          },
+        }));
+      }
+    });
+
+    // wipe previous table
+    for (const key in this.labels) {
+      delete this.labels[key];
+    }
+    this.inputsInfo.length = 0;
+
+    return issues;
+  },
+}
