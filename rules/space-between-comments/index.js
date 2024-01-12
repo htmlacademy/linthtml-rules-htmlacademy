1ef88796dbf23c5674f907a7b1e4b7b36bcbc8c9
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-01-12 09:14:42 +0300

    Adds htmlacademy/space-between-comments

--- rules/space-between-comments/index.js
+++ rules/space-between-comments/index.js
@@ -1,7 +1,15 @@
 'use strict';
 const { is_comment_node } = require('@linthtml/dom-utils');
 
-const hasSpacesAtStartAndEnd = (string) => string.startsWith(' ') && string.endsWith(' ');
+const hasSpacesAtStartAndEnd = (rule, string) => {
+  rule = rule === undefined ? 'space' : rule;
+
+  if (rule === 'space') {
+    return string.startsWith(' ') && string.endsWith(' ');
+  } else if (rule === 'no-space') {
+    return !string.startsWith(' ') && !string.endsWith(' ');
+  }
+};
 
 module.exports = {
   name: 'htmlacademy/space-between-comments',
@@ -9,12 +17,17 @@ module.exports = {
   lint(node, rule_config, { report }) {
     if (is_comment_node(node)) {
       const comment = node.data;
-      const isEdges = hasSpacesAtStartAndEnd(comment);
+      const isEdges = hasSpacesAtStartAndEnd(rule_config, comment);
 
       if (!isEdges) {
+        // eslint-disable-next-line camelcase
+        const message = rule_config === 'space'
+          ? 'The comment should contain spaces at the beginning and end of the message.'
+          : 'The comment should not contain spaces at the beginning and end of the message.';
+
         report({
           position: node.loc,
-          message: 'The comment does not contain spaces at the beginning and end of the message.'
+          message
         });
       }
     }
