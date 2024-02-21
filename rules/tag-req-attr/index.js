3f14147761e272f8e6ff41c6548d66d65bf86dc1
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-02-21 21:27:42 +0300

    Adds ignore field

--- rules/tag-req-attr/index.js
+++ rules/tag-req-attr/index.js
@@ -1,6 +1,37 @@
 'use strict';
 // eslint-disable-next-line camelcase
-const { is_tag_node, has_non_empty_attribute, has_attribute } = require('@linthtml/dom-utils');
+const { is_tag_node, has_non_empty_attribute, has_attribute, attribute_value } = require('@linthtml/dom-utils');
+const checkAttributes = (node, requiredAttributes, report) => {
+  requiredAttributes.forEach(({ name, allowEmpty, ignore }) => {
+    allowEmpty = typeof allowEmpty === 'undefined' ? false : allowEmpty;
+    if (ignore) {
+      let shouldIgnore = false;
+      for (const key in ignore) {
+        if (has_attribute(node, key) && attribute_value(node, key).chars === ignore[key]) {
+          shouldIgnore = true;
+          break;
+        }
+      }
+      if (shouldIgnore) {
+        return;
+      }
+    }
+
+    if (!has_attribute(node, name) || !has_non_empty_attribute(node, name, allowEmpty)) {
+      report({
+        code: 'E057',
+        position: node.open.loc,
+        meta: {
+          data: {
+            attribute: name,
+            tag: node.name
+          }
+        }
+      });
+    }
+  });
+};
+
 
 module.exports = {
   name: 'htmlacademy/tag-req-attr',
@@ -12,23 +43,7 @@ module.exports = {
         if (Object.hasOwnProperty.call(rule_config, tagName) && tagName === node.name) { // Ensured property belongs to object
           // eslint-disable-next-line camelcase
           const requiredAttributes = rule_config[tagName];
-
-          requiredAttributes.forEach(({ name, allowEmpty }) => {
-            allowEmpty = typeof allowEmpty === 'undefined' ? false : allowEmpty;
-
-            if (!has_attribute(node, name) || !has_non_empty_attribute(node, name, allowEmpty)) {
-              report({
-                code: 'E057',
-                position: node.open.loc,
-                meta: {
-                  data: {
-                    attribute: name,
-                    tag: node.name
-                  }
-                }
-              });
-            }
-          });
+          checkAttributes(node, requiredAttributes, report);
         }
       }
     }
