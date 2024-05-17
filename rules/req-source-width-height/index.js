004f69e7b0fbc4453c05ce5f235608a5eea97d7b
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2024-05-17 10:52:37 +0300

    Fix report in req-source-width-height

--- rules/req-source-width-height/index.js
+++ rules/req-source-width-height/index.js
@@ -4,7 +4,7 @@ const { has_non_empty_attribute, is_tag_node } = require('@linthtml/dom-utils');
 module.exports = {
   name: 'htmlacademy/req-source-width-height',
   // eslint-disable-next-line camelcase
-  lint(node, { report }) {
+  lint(node, rule_config, { report }) {
     if (is_tag_node(node) && node.name === 'picture') {
       node.children.forEach((child) => {
         if (is_tag_node(child) && child.name === 'source') {
